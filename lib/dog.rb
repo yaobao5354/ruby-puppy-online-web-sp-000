f5a0c8a98ab46de4313cb0259ae556362a1db8ec
yaoyu bao <yaobao5354@gmail.com>
2019-08-30 06:01:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+require 'pry'
+
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+     save
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.print_all
+    @@all.each {|dog| puts "#{dog.name}"}
+  end 
+  
+end 
