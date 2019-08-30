a355db58c05d4067271d77e0560c718f4f763913
yaoyu bao <yaobao5354@gmail.com>
2019-08-30 05:44:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,19 @@ class Dog
   
   @@all = []
   
+  attr_accessor :name
   
-  def initialize
-    
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each {|dog| puts "#{dog}"|
   end 
 
 end 
