2aa20f257d81b63dfaca8d176c000cc5590298e8
yaoyu bao <yaobao5354@gmail.com>
2019-08-30 05:54:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,15 +9,11 @@ class Dog
   
   def initialize(name)
     @name = name 
-     save
+    @@all << self 
   end 
   
   def self.all
     @@all
-  end 
-  
-  def save
-    @@all << self 
   end 
   
   def self.clear_all
